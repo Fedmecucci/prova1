30067997a2fa85bb2561d233ad4badd468f4bd4e
Fedmecucci <129685096+Fedmecucci@users.noreply.github.com>
2024-09-10 17:28:23 +0200

    Add files via upload
    prova

---
+++ src/main/java/it/uniroma3/siw/ProvaValidApplication.java
@@ -0,0 +1,13 @@
+package it.uniroma3.siw;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProvaValidApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProvaValidApplication.class, args);
+	}
+
+}
